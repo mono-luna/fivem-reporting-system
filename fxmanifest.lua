691d3c7f78bf1ee1489aaa0838a05f8b34fb56ef
Jakob <93420584+mono-luna@users.noreply.github.com>
2023-03-28 21:23:09 -0600

    Create fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,11 @@
+-- Made by Luna: https://github.com/mono-luna/fivem-reporting-system
+
+fx_version 'cerulean'
+game 'gta5'
+
+author 'Luna'
+description 'Reporting system, used to report players to staff'
+
+version '1.0.0'
+
+server_script 'sv.lua'
