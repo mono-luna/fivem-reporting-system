9b93394f2e267556261be3d211022f6f283c512a
Jakob <93420584+mono-luna@users.noreply.github.com>
2023-03-30 01:42:59 -0600

    Create cl.lua & Add chat suggestions

---
+++ cl.lua
@@ -0,0 +1,1 @@
+TriggerEvent('chat:addSuggestion', '/report', 'Send a report of a situation to staff',{{name="description",help="The player, or a description of what happened at a scene that broke rules."}})
